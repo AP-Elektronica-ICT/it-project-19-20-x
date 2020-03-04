f6b1babcf1424718f6f8d518f0f822fa5f5ed4ea
MelvinGoorden <43994641+MelvinGoorden@users.noreply.github.com>
2020-03-04 10:06:33 +0100

    nodejs server opzetten

---
+++ server.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+//const request = require('request');
+//const fetch = require('node-fetch');
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', (req, res) => {
+    res.sendfile("index.html")
+    }
+);
+
+app.listen(app.get('port'), () => {
+    console.log(`Express started on http://localhost:${
+      app.get('port')}; press Ctrl-C to terminate.`);
+  });
+
+
+
+// Fetching all data from GEOJSON, Filtering should be added later on
+/*fetch('https://geodata.antwerpen.be/arcgissql/rest/services/P_Portal/portal_publiek4/MapServer/292/query?where=1%3D1&outFields=*&outSR=4326&f=json')
+.then(function(response){
+    return response.json();
+})
+.then(function(json){
+    console.log(json);
+});
+*/
+
+
+
