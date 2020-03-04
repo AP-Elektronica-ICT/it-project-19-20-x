810e6bf8bc24a6e6293bdac7a0671cb815ddc856
LarsDMSoftware <larsdemaere@hotmail.com>
2020-03-04 10:53:40 +0100

    Leaflet Map Fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,5 @@
-let mymap = L.map('map').setView([42.35, -71.08], 13);
+var map = L.map('map').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-  maxZoom: 18,
-  id: 'mapbox/streets-v11',
-  zoomOffset: -1,
-  accessToken: 'pk.eyJ1IjoibGVjdG9yd291dGVyIiwiYSI6ImNrM29pY2dqdTFlc24zbW5tcjZ0bHcyMmYifQ.J3VURMKB4XSNnXz-iqubsQ'
-}).addTo(mymap);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright%22%3EOpenStreetMap</a> contributors'
+}).addTo(map);
