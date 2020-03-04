fe2c5eeef9bf28bf70ed2ecc0e90ff95c4b5e434
LarsDMSoftware <larsdemaere@hotmail.com>
2020-03-04 10:20:13 +0100

    Leaflet map init

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,9 @@
-var cultuurLocatiesGeoJson = 'cultuurlocaties.geojson';
+let mymap = L.map('map').setView([42.35, -71.08], 13);
 
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-
-
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  id: 'mapbox/streets-v11',
+  zoomOffset: -1,
+  accessToken: 'pk.eyJ1IjoibGVjdG9yd291dGVyIiwiYSI6ImNrM29pY2dqdTFlc24zbW5tcjZ0bHcyMmYifQ.J3VURMKB4XSNnXz-iqubsQ'
 }).addTo(mymap);
-
-
-var marker = L.marker([51.5, -0.09]).addTo(mymap);
-
-L.geoJSON(geojsonFeature).addTo(mymap);
-
