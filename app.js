e300760d9025becb9526ee1912d22e7e410b3c8c
Saifeddine Legsir <s114821@ap.be>
2020-03-08 11:36:08 +0100

    css + html

--- app.js
+++ app.js
@@ -1,32 +1,16 @@
-const http = require('http');
 const express = require('express');
+const ejs = require('ejs');
+
 const app = express();
-//const request = require('request');
-//const fetch = require('node-fetch');
-
 app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => {
-    res.sendfile("index.html")
-    }
-);
+app.get('/', (req,res) => {
+res.render('index')
+});
 
 app.listen(app.get('port'), () => {
-    console.log(`Express started on http://localhost:${
-      app.get('port')}; press Ctrl-C to terminate.`);
-  });
-
-
-
-// Fetching all data from GEOJSON, Filtering should be added later on
-/*fetch('https://geodata.antwerpen.be/arcgissql/rest/services/P_Portal/portal_publiek4/MapServer/292/query?where=1%3D1&outFields=*&outSR=4326&f=json')
-.then(function(response){
-    return response.json();
-})
-.then(function(json){
-    console.log(json);
+  console.log(`Express started on http://localhost:${
+    app.get('port')}; press Ctrl-C to terminate.`);
 });
-*/
-
-
-
