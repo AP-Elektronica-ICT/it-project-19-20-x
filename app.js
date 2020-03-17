e0e0bff9d86045776cd48f0de645e9ed5973f69b
LarsDMSoftware <larsdemaere@hotmail.com>
2020-03-17 15:18:35 +0100

    Pre-Merge Push

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
-
 const express = require('express');
-const fetch = require('fetch');
+const fetch = require('node-fetch');
 const ejs = require('ejs');
 
+//API fetch
+let jsonData;
+
 const app = express();
-app.get('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+
 
 fetch('https://geodata.antwerpen.be/arcgissql/rest/services/P_Portal/portal_publiek4/MapServer/292/query?where=1=1&outFields=*&outSR=4326&f=json')
   .then((response) => {
     return response.json();
   })
   .then((data) => {
-    // jsonData = JSON.stringify(data);
+    jsonData = JSON.stringify(data);
     app.get('/', (req,res) => {
       res.render('index', {'jsonData': JSON.stringify(data)})
       });
   });
 
 
-app.listen(app.get('port'), () =>
+app.listen(app.get('port'), () => 
 {
   console.log(`Express Started on http://localhost:${
     app.get('port')}; press Ctrl-c to terminate.`);
