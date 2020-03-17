363987b52a70c2e569ed1139fea39dc0f6235988
LarsDMSoftware <larsdemaere@hotmail.com>
2020-03-17 01:10:49 +0100

    API en Stijl samenvoegen (Probleem met App.js voor mij)

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
+const http = require('http');
 const express = require('express');
+const fetch = require('node-fetch');
 const ejs = require('ejs');
 
 const app = express();
-app.set('port', process.env.PORT || 3000);
-app.set('view engine', 'ejs');
+app.get('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req,res) => {
-res.render('index')
-});
+fetch('https://geodata.antwerpen.be/arcgissql/rest/services/P_Portal/portal_publiek4/MapServer/292/query?where=1=1&outFields=*&outSR=4326&f=json')
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    // jsonData = JSON.stringify(data);
+    app.get('/', (req,res) => {
+      res.render('index', {'jsonData': JSON.stringify(data)})
+      });
+  });
 
-app.listen(app.get('port'), () => {
-  console.log(`Express started on http://localhost:${
-    app.get('port')}; press Ctrl-C to terminate.`);
+
+app.listen(app.get('port'), () =>
+{
+  console.log(`Express Started on http://localhost:${
+    app.get('port')}; press Ctrl-c to terminate.`);
 });
--- public/mapScript.js
+++ public/mapScript.js
@@ -1,5 +1,14 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([51.2171918, 4.4212529], 10);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright%22%3EOpenStreetMap</a> contributors'
 }).addTo(map);
+
+// for(var k in jsonData) {
+//     console.log(jsonData.features[k].geometry.x);
+// }
+console.log(jsonData.features[0].geometry.X);
+
+L.geoJSON(jsonData,{onEachFeatures: function (feature, layer) {
+    layer.bindPopup(feature.attributes.naam);
+}}).addTo(map);
