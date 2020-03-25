b82274133ae708e2c36ee71388abc316444c317d
LarsDMSoftware <Larsdemaere@hotmail.com>
2020-03-25 07:12:54 +0100

    Update mapScript.js

--- public/mapScript.js
+++ public/mapScript.js
@@ -4,50 +4,52 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright%22%3EOpenStreetMap</a> contributors'
 }).addTo(map);
 
-var myLayer = L.geoJSON().addTo(map);
+
+  var myLayer = L.geoJSON().addTo(map);
 
 
-// for(var k in jsonData) {
-//     console.log(jsonData.features[k].geometry.x);
-// }
-//console.log(jsonData.features[0].geometry.X);
-
-
-async function getJSON_Data()
-{
-    const url ="/JSONdata";
-    const response = await fetch(url);
-    const json_Data = await response.json();
-    return json_Data;
-}
-
-
-
-getJSON_Data().then(data => 
-{
-    const cultuurLocatieData = data[0].features;
-    const erfgoodLocatieData = data[1].features;
-    console.log("test");
-    //Functies om locaties te tonen op roepen
-    Locaties(cultuurLocatieData);
-    Locaties(erfgoodLocatieData);
-})
-
-
-// Toon functies voor locaties zullen hier komen
-function Locaties(data)
-{
-    L.geoJSON(data,{onEachFeatures: function (feature, layer) {
-        L.marker([features.attributes.geometry.x,features.attributes.geometry.y]);
-        //myLayer.bindPopup(feature.attributes.naam);
-    }}).addTo(map);
-    console.log("test2");
-}
-/*
-function erfgooedLocaties(data)
-{
-    L.geoJSON(data ,{onEachFeatures: function (feature, layer) {
-        layer.bindPopup(feature.attributes.naam);
-    }}).addTo(map);
-}
-*/
+  // for(var k in jsonData) {
+  //     console.log(jsonData.features[k].geometry.x);
+  // }
+  //console.log(jsonData.features[0].geometry.X);
+  
+  
+  async function getJSON_Data()
+  {
+      const url ="/JSONdata";
+      const response = await fetch(url);
+      const json_Data = await response.json();
+      return json_Data;
+  }
+  
+  
+  
+  getJSON_Data().then(data => 
+  {
+      const cultuurLocatieData = data[0].features;
+      const erfgoodLocatieData = data[1].features;
+      console.log("test");
+      //Functies om locaties te tonen op roepen
+      Locaties(cultuurLocatieData);
+      Locaties(erfgoodLocatieData);
+  })
+  
+  
+  // Toon functies voor locaties zullen hier komen
+  function Locaties(data)
+  {
+      L.geoJSON(data,{onEachFeatures: function (feature, layer) {
+          L.marker([features.attributes.geometry.x,features.attributes.geometry.y]);
+          //myLayer.bindPopup(feature.attributes.naam);
+      }}).addTo(map);
+      console.log("test2");
+  }
+  /*
+  function erfgooedLocaties(data)
+  {
+      L.geoJSON(data ,{onEachFeatures: function (feature, layer) {
+          layer.bindPopup(feature.attributes.naam);
+      }}).addTo(map);
+  }
+  */
+  
