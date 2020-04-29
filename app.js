8adee6e878001d87d326c7dc8a5ea070bcc41be4
Ayoubsafri <60689171+Ayoubsafri@users.noreply.github.com>
2020-04-29 23:27:43 +0200

    no message

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ app.listen(app.get('port'), () =>
 
 
 
-/*
+/* 
  // Cultuurlocaties
  const responeCultuur = await fetch('https://geodata.antwerpen.be/arcgissql/rest/services/P_Portal/portal_publiek4/MapServer/292/query?where=1%3D1&outFields=*&outSR=4326&f=json')
  .then((response) => {
