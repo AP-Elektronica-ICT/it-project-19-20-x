ee3d15e3479371b48a2616767363c4a2167968ff
MelvinGoorden <43994641+MelvinGoorden@users.noreply.github.com>
2020-05-19 23:47:09 +0200

    cleanup

--- app.js
+++ app.js
@@ -5,21 +5,6 @@ const ejs = require('ejs');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 
-//Requires  voor Database
-const {
-  MongoClient
-} = require('mongodb');
-const uri = 'mongodb+srv://Lars:APLabo@oefeningenwebont-fdenf.mongodb.net/test?retryWrites=true&w=majority';
-const DATABASE = 'IT-Project-X';
-const COLLECTION_DATABASE = 'Users';
-var mongoose = require('mongoose');
-
-let dataCultuur;
-let dataErfgoed;
-
-//API fetch
-let jsonData;
-
 const app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
@@ -29,7 +14,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.get('/', (req, res) => {
   res.render('index')
 });
-
 
 // Views voor de andere paginas
 // Contact pagina
@@ -41,6 +25,7 @@ app.get('/login', (req, res) => {
   res.render('login')
 });
 
+//load apidata
 async function apistart() {
   const erfUrl = "https://geodata.antwerpen.be/arcgissql/rest/services/P_Portal/portal_publiek4/MapServer/293/query?where=1=1&outFields=*&outSR=4326&f=json";
   const erfResponse = await fetch(erfUrl);
@@ -63,6 +48,7 @@ async function apistart() {
 
 apistart();
 
+//contact email
 app.post('/send-email', (req, res) => {
 
   // Instantiate the SMTP server
@@ -78,7 +64,7 @@ app.post('/send-email', (req, res) => {
 
   // Specify what the email will look like
   const mailOpts = {
-    from: 'Your sender info here', // This is ignored by Gmail
+    from: 'Your sender info here',
     to: "opdrachtcms.ap@gmail.com",
     subject: 'New message from contact form',
     text: `${req.body.firstname} (${req.body.lastname}) says: ${req.body.subject}`
@@ -93,69 +79,3 @@ app.listen(app.get('port'), () => {
   console.log(`Express Started on http://localhost:${
     app.get('port')}; press Ctrl-c to terminate.`);
 });
-
-// Database Code
-/*
-app.post('/signup', function (req,res,next)
-{
-  let user =
-  {
-    Name: req.body.name,
-       Email: req.body.email,
-       Pass: req.body.pass,
-  };
-
-  var UserReg = mongoose.model('UserReg', UserSchema);
-   UserReg.create(user, function(err, newUser) {
-      if(err) return next(err);
-      req.session.user = email;
-      return res.send('Logged In!');
-   });
-
-});
-
-mongoose.connect(uri, {useUnifiedTopology: true ,useNewUrlParser: true } );
-
-let db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  // we're connected!
-});
-
-let Schema = mongoose.Schema;
-
-// Schema
-let UserRegSchema = mongoose.Schema
-({
-  Name: String,
-  Email: String,
-  Pass: String,
-  Num: Number,
-  Favourites: 
-      [{ 
-        objID: Number
-      }]
-});
-
-// Model
-let UserReg = mongoose.model('UserReg', UserRegSchema);
-
-// Add in collection
-let UserAdd = new UserReg({
-  Name: req.body.name,
-  Email: req.body.email,
-  Password: req.body.pass,
-});
-
-// Save
-UserAdd.save(function (err, fluffy) {
-  if (err) return console.error(err);
-});
-
-function CheckUserExsist(givenUserName)
-{
-  UserSchema.findOne({userName: givenUserName})
-
-}
-
-*/
